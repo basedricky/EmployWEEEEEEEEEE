d3e74eb41f0c22e577e17f7fd2bb5876fa9ff71d
Ricky Panh <ricky@dragonflyut.com>
2021-04-16 00:55:45 -0600

    created viewAllEmployees function

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+const consoleTable = require('console.table');
+const promisemysql = require("promise-mysql");
 
-const conneection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -12,9 +13,9 @@ const conneection = mysql.createConnection({
     database: "employee_db"
 });
 
-conneection.connect(function (err) {
-    if (err) throw err
-    console.log("Connected as Id" + conneection.threadId)
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected as Id" + connection.threadId)
     startPrompt();
 });
 
@@ -23,14 +24,14 @@ conneection.connect(function (err) {
 function startPrompt() {
     inquirer.prompt({
 
-
+        name: "action",
         type: "list",
         message: "What would you like to do?",
         choices: [
 
             "View all employees.",
             "View all employees by Role.",
-            "View all employees by Deparments.",
+            "View all employees by Departments.",
             "Update employee Role."
         ]
     })
@@ -54,8 +55,30 @@ function startPrompt() {
 
                 case "Update employee Role.":
                     updateEmployeeRole();
+                    break;
             }
 
-
         });
+}
+
+// view all employees 
+
+function viewAllEmployees() {
+
+    // query to view all employees
+
+    let query = '"SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salaryFROM employee e INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";';
+
+    // Query from connection
+    connection.query(query, function (err, res) {
+        if (err) return err;
+        console.log("\n");
+
+        // display query results using console.table
+        console.table(res);
+
+        // this will take us back to the beginning
+        mainMenu();
+    });
+
 }
