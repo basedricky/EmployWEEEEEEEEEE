25f61e02a61a57342f1fa16a5702fc66959d358d
Ricky Panh <ricky@dragonflyut.com>
2021-04-15 23:02:41 -0600

    fixed some spelling errors

--- schema.sql
+++ schema.sql
@@ -3,21 +3,21 @@ USE employee_db;
 CREATE TABLE department (
   id INT AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
-  PRIMARY KEYY (id)
+  PRIMARY KEY (id)
 );
 CREATE TABLE role (
   id INT AUTO_INCREMENT,
   title VARCHAR (30) NOT NULL,
   salary DECIMAL (6, 2) NOT NULL,
-  department_id ID NOT NULL,
+  department_id INT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY(department_id) REFERENCES department(id)
 );
 CREATE TABLE employee (
   id INT AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
-  last_name CARCHAR(30) NOT NULL,
-  rolde_id INT NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
   manater_id INT,
   PRIMARY KEY(id),
   FOREIGN KEY (role_id) REFERENCES role(id),
